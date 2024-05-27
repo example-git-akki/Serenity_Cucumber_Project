13df30547e0a08968024ad629c8fb58c9ab15eef
AkashGitBeware <akpatil002325@gmail.com>
2024-05-27 10:31:58 +0530

    changes added

--- src/test/java/TestRunner/Runner.java
+++ src/test/java/TestRunner/Runner.java
@@ -4,6 +4,9 @@ import io.cucumber.junit.CucumberOptions;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
+
+//This is the Runner configuration
+
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(features = "src/test/resources/features",
         glue = "stepDefinitions"
