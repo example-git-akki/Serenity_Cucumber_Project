4d80d91f3a9f7d6db475ca213cc27a2c1e70d45e
AkashGitBeware <akpatil002325@gmail.com>
2024-05-30 12:25:20 +0530

    trying to commit

--- src/test/java/TestRunner/Runner.java
+++ src/test/java/TestRunner/Runner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(features = "src/test/resources/features",
         glue = "stepDefinitions"
-//        plugin = {"pretty"}, monochrome = true
+
 )
 public class Runner {
 }
